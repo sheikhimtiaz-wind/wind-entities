514c1db5d8a7224348e12989aa67e72603c26a7a
Sheikh Imtiaz Ahmed <imticorei54@gmail.com>
2024-06-11 12:45:43 +0600

    update package

--- src/main/java/com/sheikhimtiaz/windentities/WindEntitiesApplication.java
+++ src/main/java/com/sheikhimtiaz/windentities/WindEntitiesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommonlibsApplication {
+public class WindEntitiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonlibsApplication.class, args);
+		SpringApplication.run(WindEntitiesApplication.class, args);
 	}
 
 }
